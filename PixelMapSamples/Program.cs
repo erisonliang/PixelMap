e398b96642fa303728973dd7ed0de4051de87b32
ruarai <ruarai.big@gmail.com>
2015-10-03 17:22:49 +1000

    add sample program

---
+++ PixelMapSamples/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PixelMapSharp;
+
+namespace PixelMapSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quickly load a PixelMap through a Bitmap
+            PixelMap map = new PixelMap(new Bitmap("Lenna.png"));
+
+            for (int x = 0; x < map.Width; x++)
+            {
+                for (int y = 0; y < map.Height; y++)
+                {
+                    //Sample a pixel
+                    Pixel pixel = map[x, y];
+
+                    //Create a hue value
+                    float value = ((float)x/map.Width)*360f;
+
+                    //Set the hue value to our sample
+                    pixel.Hue = value;
+
+                    //Return our sample to the PixelMap
+                    map[x, y] = pixel;
+                }
+            }
+
+            //Save the PixelMap through a Bitmap
+            map.GetBitmap().Save("output.png");
+        }
+    }
+}
